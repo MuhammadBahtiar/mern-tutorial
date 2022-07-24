11a37cf6b9dc185dd6b4ee4d7d160d8f982a2647
muhammadbahtiar <bahtiarm27@gmail.com>
2022-07-24 14:27:25 +0700

    initial commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 const {
     registerUser,
     loginUser,
-    getMe } = require('../controllers/userController')
+    getMe } = require('../controllers/userController');
+const { protect } = require("../middleware/authMiddleware");
 
-router.post('/',registerUser)
-router.post('/login', loginUser)
-router.get('/me', getMe)
+router.post('/',registerUser);
+router.post('/login', loginUser);
+router.get('/me', protect, getMe);
 
-module.exports = router
+module.exports = router;
